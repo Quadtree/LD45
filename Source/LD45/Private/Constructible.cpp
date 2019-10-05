7fca9c641147450b1f7a05dc02a899f70fb1be19
Quadtree <quadtree@gmail.com>
2019-10-05 13:54:07 -0700

    Constructible added

---
+++ Source/LD45/Private/Constructible.cpp
@@ -0,0 +1,5 @@
+#include "Constructible.h"
+#include "Constructible.ac.h"
+
+extends(AActor)
+
---
+++ Source/LD45/Public/Constructible.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "EngineMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Constructible.generated.h"
+
+UCLASS()
+class LD45_API AConstructible : public AActor
+{
+	GENERATED_BODY()
+};
